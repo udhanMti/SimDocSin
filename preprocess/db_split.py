61f2adaa24354a08c02321c97a6745e545d2c041
JanakaSandaruwan <janakasandaruwan1996@gmail.com>
2021-02-17 20:01:58 +0530

    add cmd to db_split.py

--- preprocess/db_split.py
+++ preprocess/db_split.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import os
+import sys
 # from extract_ne import extract_names, extract_designations
 from extract_digits import extract_digits
 from datetime import datetime
@@ -8,7 +9,9 @@ from preprocess.filename import get_file_paths
 
 start = datetime.now()
 
-en_digits = []
+args = sys.argv
+lang = args[1]
+
 si_digits = []
 
 # en_names = []
@@ -28,53 +31,9 @@ for file_name in paths:
     for j in range(limit):
         ind = str(i // 1000)
         print(ind + " " + str(i % 1000))
-        if i < 0:
-
-            #save english embeddings
-            filename = "../db/" + ind + "/en/" + str(i % 1000)
-            if not os.path.exists(os.path.dirname(filename)):
-                try:
-                    os.makedirs(os.path.dirname(filename))
-                except OSError as exc:  # Guard against race condition
-                    if exc.errno != errno.EEXIST:
-                        raise
-
-            b = np.array(data[j]["embed_en"])
-            np.save(filename, b)
-
-
-            # save english weightings
-            filename = "../db/" + ind + "/wen/" + str(i % 1000)
-            if not os.path.exists(os.path.dirname(filename)):
-                try:
-                    os.makedirs(os.path.dirname(filename))
-                except OSError as exc:  # Guard against race condition
-                    if exc.errno != errno.EEXIST:
-                        raise
-
-            b = np.array(data[j]["weight_en"])
-            np.save(filename, b)
-
-            doc_en = data[j]["content_en"]
-            en_digits.append(extract_digits(doc_en, 'en'))
-            # en_names.append(extract_names(doc_en,'en'))
-            # en_designations.append(extract_designations(doc_en,'en'))
-            # en_sent_count.append(len(doc_to_sentence(doc_en,'en')))
-
-            # save english documents
-            filename = "../db/" + ind + "/docen/"
-            if not os.path.exists(os.path.dirname(filename)):
-                try:
-                    os.makedirs(os.path.dirname(filename))
-                except OSError as exc:  # Guard against race condition
-                    if exc.errno != errno.EEXIST:
-                        raise
-            f = open("../db/" + ind + "/docen/" + str(i % 1000) + ".txt", "w")
-            f.write(doc_en)
-            f.close()
 
         #save sinhala embeddings
-        filename = "../db/" + ind + "/si/" + str(i % 1000)
+        filename = "../db/" + ind + "/"+lang+"/" + str(i % 1000)
         if not os.path.exists(os.path.dirname(filename)):
             try:
                 os.makedirs(os.path.dirname(filename))
@@ -82,12 +41,12 @@ for file_name in paths:
                 if exc.errno != errno.EEXIST:
                     raise
 
-        b = np.array(data[j]["embed_si"])
+        b = np.array(data[j]["embed_"+lang])
         np.save(filename, b)
 
 
         # save sinhala weightings
-        filename = "../db/" + ind + "/wsi/" + str(i % 1000)
+        filename = "../db/" + ind + "/w"+lang+"/" + str(i % 1000)
         if not os.path.exists(os.path.dirname(filename)):
             try:
                 os.makedirs(os.path.dirname(filename))
@@ -95,21 +54,21 @@ for file_name in paths:
                 if exc.errno != errno.EEXIST:
                     raise
 
-        b = np.array(data[j]["weight_si"])
+        b = np.array(data[j]["weight_"+lang])
         np.save(filename, b)
 
-        doc_si = data[j]["content_si"]
-        si_digits.append(extract_digits(doc_si, 'si'))
+        doc_si = data[j]["content_"+lang]
+        si_digits.append(extract_digits(doc_si, lang))
 
         # save sinhala document
-        filename = "../db/" + ind + "/docsi/"
+        filename = "../db/" + ind + "/doc"+lang+"/"
         if not os.path.exists(os.path.dirname(filename)):
             try:
                 os.makedirs(os.path.dirname(filename))
             except OSError as exc:  # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
-        f = open("../db/" + ind + "/docsi/" + str(i % 1000) + ".txt", "w", encoding='utf-8')
+        f = open("../db/" + ind + "/doc"+lang+"/" + str(i % 1000) + ".txt", "w", encoding='utf-8')
         f.write(doc_si)
         f.close()
         i = i + 1
@@ -128,7 +87,7 @@ for file_name in paths:
 
 
 # save sinhala digits
-filename = "../db/si_digits"
+filename = "../db/"+lang+"_digits"
 if not os.path.exists(os.path.dirname(filename)):
     try:
         os.makedirs(os.path.dirname(filename))
@@ -139,17 +98,6 @@ if not os.path.exists(os.path.dirname(filename)):
 b = np.array(si_digits)
 np.save(filename, b)
 
-# save english digits
-filename = "../db/en_digits"
-if not os.path.exists(os.path.dirname(filename)):
-    try:
-        os.makedirs(os.path.dirname(filename))
-    except OSError as exc:  # Guard against race condition
-        if exc.errno != errno.EEXIST:
-            raise
-
-b = np.array(en_digits)
-np.save(filename, b)
 
 # save english names
 # filename = "./army/en_names"
