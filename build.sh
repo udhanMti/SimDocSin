b09bb1ad29fa2e98cf7c239d427f46307b57e75b
JanakaSandaruwan <janakasandaruwan1996@gmail.com>
2021-02-18 20:56:41 +0530

    Add shell file for creating folders

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir -p inputs
+mkdir -p db
+mkdir -p Embeddings/Sinhala
+mkdir -p Embeddings/English
+mkdir -p Embeddings/Parallel
+mkdir -p outputs_full_match
+mkdir -p outputs_partial_match
+mkdir -p index
