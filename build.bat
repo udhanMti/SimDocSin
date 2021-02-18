ef4ba6e00fd828562a4596928d6b4bb15420c882
UdeshAthukorala <udeshathukorala@gmail.com>
2021-02-18 22:52:35 +0530

    Update build.bat and build.sh

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 if not exist inputs mkdir inputs
 if not exist db mkdir db
-if not exist Embeddings mkdir Embeddings
-cd Embeddings
-if not exist Sinhala mkdir Sinhala
-if not exist English mkdir English
-if not exist Parallel mkdir Parallel
+if not exist embeddings mkdir embeddings
+cd embeddings
+if not exist sinhala mkdir sinhala
+if not exist english mkdir english
+if not exist parallel mkdir parallel
 cd ..
 if not exist outputs_full_match mkdir outputs_full_match
 if not exist outputs_partial_match mkdir outputs_partial_match
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 mkdir -p inputs
 mkdir -p db
-mkdir -p Embeddings/Sinhala
-mkdir -p Embeddings/English
-mkdir -p Embeddings/Parallel
+mkdir -p embeddings/sinhala
+mkdir -p embeddings/english
+mkdir -p embeddings/parallel
 mkdir -p outputs_full_match
 mkdir -p outputs_partial_match
 mkdir -p index
