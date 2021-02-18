1a1f292845d8ff12cd0e89ee3455db0c471d5a1d
JanakaSandaruwan <janakasandaruwan1996@gmail.com>
2021-02-18 20:26:40 +0530

    Add windows bat file for creating folders

---
+++ build.bat
@@ -0,0 +1,11 @@
+if not exist inputs mkdir inputs
+if not exist db mkdir db
+if not exist Embeddings mkdir Embeddings
+cd Embeddings
+if not exist Sinhala mkdir Sinhala
+if not exist English mkdir English
+if not exist Parallel mkdir Parallel
+cd ..
+if not exist outputs_full_match mkdir outputs_full_match
+if not exist outputs_partial_match mkdir outputs_partial_match
+if not exist index mkdir index
