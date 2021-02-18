2e12052468e78f5803ff1396736dc70514a68f9a
UdeshAthukorala <udeshathukorala@gmail.com>
2021-02-18 22:21:51 +0530

    Update folder names in filename.py

--- preprocess/filename.py
+++ preprocess/filename.py
@@ -5,7 +5,7 @@ def get_file_paths(lang):
     path_parent = pathlib.Path(__file__).parents[1]
 
     # define the embedding folder
-    path_embeddings = path_parent / 'Embeddings'
+    path_embeddings = path_parent / 'embeddings'
 
     # define the pattern
     pattern = "*.json"
@@ -13,7 +13,7 @@ def get_file_paths(lang):
     paths = []
 
     # define the Parallel embedding folder
-    path_parallel = path_embeddings / 'Parallel'
+    path_parallel = path_embeddings / 'parallel'
 
     ### ADD Parallel Documents for both en and si
     for currentFile in path_parallel.glob(pattern):
@@ -21,7 +21,7 @@ def get_file_paths(lang):
 
     if lang == 'si':
         # define the Sinhala embedding folder
-        path_sinhala = path_embeddings / 'Sinhala'
+        path_sinhala = path_embeddings / 'sinhala'
 
         ### ADD Sinhala Documents
         for currentFile in path_sinhala.glob(pattern):
@@ -30,7 +30,7 @@ def get_file_paths(lang):
 
     else:
         # define the English embedding folder
-        path_english = path_embeddings / 'English'
+        path_english = path_embeddings / 'english'
 
         ### ADD English Documents
         for currentFile in path_english.glob(pattern):
